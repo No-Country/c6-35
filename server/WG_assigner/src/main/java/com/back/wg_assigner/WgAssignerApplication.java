db7a7d8969889c2c5edb113647bccded3de35266
Noblega_Diego <diegomendoza20015@gmail.com>
2022-08-06 15:55:42 -0300

    init back

---
+++ server/WG_assigner/src/main/java/com/back/wg_assigner/WgAssignerApplication.java
@@ -0,0 +1,13 @@
+package com.back.wg_assigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgAssignerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WgAssignerApplication.class, args);
+    }
+
+}
---
+++ server/WG_assigner/src/test/java/com/back/wg_assigner/WgAssignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.wg_assigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgAssignerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
