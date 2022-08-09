a3f4c10b3bf5baf6fd6be003694b76e2c2a4ac4f
Noblega_Diego <diegomendoza20015@gmail.com>
2022-08-09 01:34:35 -0300

    add: Employee Entity, repository and BaseEntity

---
+++ server/WG_assigner/src/main/java/com/back/wg_assigner/entities/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.back.wg_assigner.entities;
+
+
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date created;
+    private Date updated;
+    private boolean deleted;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated = new Date();
+    }
+
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    protected void onDeleted(){
+        this.deleted = false;
+    }
+}
---
+++ server/WG_assigner/src/main/java/com/back/wg_assigner/entities/Employee.java
@@ -0,0 +1,30 @@
+package com.back.wg_assigner.entities;
+
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "personal")
+@SQLDelete(sql = "update personal SET deleted = 1 where ID=?")
+@Where(clause = "deleted <> 1")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee extends BaseEntity{
+    @Column(name = "nombre")
+    private String name;
+    @Column(name = "apellido")
+    private String lastname;
+    @Column(name = "dni")
+    private Integer dni;
+    @Column(name = "celular")
+    private Integer phone;
+    @Column(name = "direccion")
+    private String direccion;
+    @Column(name = "legajo")
+    private Integer employeeId;
+}
