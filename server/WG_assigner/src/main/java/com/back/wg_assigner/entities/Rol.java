bba10dcebb8a83d4292d3ea5dc25f7da5c6781cd
Torales Andres <andres.toraless@gmail.com>
2022-08-12 15:42:14 -0300

    rol entiti and repository

--- server/WG_assigner/src/main/java/com/back/wg_assigner/entities/Rol.java
+++ server/WG_assigner/src/main/java/com/back/wg_assigner/entities/Rol.java
@@ -1,4 +1,6 @@
 package com.back.wg_assigner.entities;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,25 +10,23 @@ import org.hibernate.annotations.Where;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "Rol")
 @SQLDelete(sql = "update Users SET deleted = 1 where ID=?")
 @Where(clause = "deleted <> 1")
 @Data   //ges set
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User  extends BaseEntity{
-    @Column(name = "nombre")
-    private String userName;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "password")
-    private String password;
-    @Column(name = "rol")
-    @ManyToOne
-    private Rol rol;
+public class Rol  extends BaseEntity {
+    @Column(name = "denomination")
+    private String denomination;
+    @Column(name = "description")
+    private String  description;
+    @OneToMany
+    private List<User> users;
 }
--- server/WG_assigner/src/main/java/com/back/wg_assigner/repositories/RolRepository.java
+++ server/WG_assigner/src/main/java/com/back/wg_assigner/repositories/RolRepository.java
@@ -1,9 +1,10 @@
 package com.back.wg_assigner.repositories;
 
-import com.back.wg_assigner.entities.User;
+import com.back.wg_assigner.entities.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface RolRepository extends JpaRepository<Rol, Long> {
 }
+
