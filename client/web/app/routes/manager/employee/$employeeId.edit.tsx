89b86d591d7024097db4b8794698a9be23891c88
Noblega_Diego <diegomendoza20015@gmail.com>
2022-08-27 11:03:15 -0300

    Fe 01 panel de administración (#14)
    * panel-administración-employee y menu de navegación lateral

--- client/web/app/routes/manager/employee/$employeeId.edit.tsx
+++ client/web/app/routes/manager/employee/$employeeId.edit.tsx
@@ -1,9 +1,10 @@
-import { ActionFunction, json, redirect } from "@remix-run/node";
-import { Form, useActionData, useOutletContext, useParams } from "@remix-run/react";
+import { ActionFunction, json, LoaderFunction, redirect } from "@remix-run/node";
+import { Form, useActionData, useCatch, useLoaderData, useOutletContext, useParams } from "@remix-run/react";
 import { useEffect, useState } from "react";
 import Button from "~/components/Button";
+import ScreenCatch from "~/components/ScreenCatch";
 import TitleDirectory from "~/components/TitleDirectory";
-import { createEmployee, EmployeeModel } from "~/server/employee.server";
+import { createEmployee, EmployeeModel, getEmployeeWhitId } from "~/server/employee.server";
 import { validateName, validateDNI, validateNroLegajo, validatePhone, validateEmail, validatePassword} from "~/utils/validate";
 import Employee from "../employee";
 
@@ -23,8 +24,24 @@ interface Errors {
 
 interface ActionData {
     errors?: Errors
-  }
-
+}
+export const loader: LoaderFunction =async ({params}) => {
+    const {employeeId} = params;
+    let employee: EmployeeModel;
+    if(employeeId === "-11"){
+        employee = {id: 0, dni:"123333", name:"Diego", lastname:"Noblega", phone:2333223, employeeId:2333,direccion:"", user:{email:"diego2212@gmail.com", userName:"diegoNoble", rol:{id:2}, password:"dwdwd"}}
+        return json(employee)
+    }
+    try {
+        let data:any = await getEmployeeWhitId(employeeId);
+        if(data.error)
+            throw {error:data.error, status: data.status, message: data.message};
+        employee = data;
+        return json({data:employee})
+    } catch (error) {
+        throw new Response("No encontrado", { status: 404 });
+    }
+}
 
 export const action: ActionFunction =async ({params, request}) => {
     const form = await request.formData()
@@ -39,6 +56,7 @@ export const action: ActionFunction =async ({params, request}) => {
     const email = form.get("email")?.toString();
     const password = form.get("password")?.toString();
     const rol = form.get("rol")?.toString();
+    console.log(name)
     let errors:Errors = {};
     let actionData: ActionData = {errors};
     let isErrors = false;
@@ -67,7 +85,7 @@ export const action: ActionFunction =async ({params, request}) => {
         dni:dni!,
         employeeId:parseInt(nrolegajo!),
         phone:parseInt(celular!),
-        direccion:direccion!,
+        direccion:"",
         user:{
             userName: user!,
             email:email!,
@@ -75,29 +93,49 @@ export const action: ActionFunction =async ({params, request}) => {
             rol:{id:parseInt(rol!)}
         }
     })
-    console.log(newEmployee)
-    return redirect(`/manager/employee/${newEmployee.id!}?created=y`);
+    return redirect(`/employee/${newEmployee.id!}`);
 }
 
 
-function Input({title, value, type, name, errMsj}:{title:string, value:any, type: string, name: string, errMsj: String | undefined | null}) {
+function Input({title, value, type, name, errMsj, onChange}:{title:string, value:any, type: string, name: string, errMsj: String | undefined | null, onChange?:Function}) {
 
     return (<div className="flex flex-col mt-2">
         <label className="text-[0.8rem]">{title}</label>
-        <input type={type} className="border-app-w-write-gris border-solid border-2 px-2 py-1 rounded" name={name} aria-invalid={errMsj? true:undefined} aria-details={name+"-error"}></input>
+        <input type={type} value={value} className="border-app-w-write-gris border-solid border-2 px-2 py-1 rounded" name={name} aria-invalid={errMsj? true:undefined} aria-details={name+"-error"} 
+            onChange={event=>{
+                if(onChange)
+                    onChange(event.target.value)
+            }} ></input>
         {(errMsj)?
             <span id={name+"-error"} className="text-[0.7rem] text-[#EF233C]">{errMsj}</span>:
             <></>}
     </div>)
 }
 
+export function CatchBoundary() {
+    const caught = useCatch();
+    const data = caught.data;
+    return (
+      <ScreenCatch data={data} status={caught.status}></ScreenCatch>
+    );
+  }
 
-export default function AddEmployee() {
+export default function EditEmployee() {
+    const {data:employee} = useLoaderData() as {data:EmployeeModel};
+    const [data, setData] = useState(employee);
     const actionData = useActionData() as ActionData;
-    
+    console.log(data)
+    const handle = (callBack:(data:EmployeeModel, value:any)=>void)=>{
+        return (value:any)=>{
+            let d = {...data};
+            callBack(d, value);
+            setData(d);
+        }
+    }
+
 
     return (<>
-        <div className="p-2 md:px-4">
+        <div className="p-2 ">
             <div className="text-2xl mb-2">
                 <h1>Alta de empleado</h1>
             </div>
@@ -111,16 +149,16 @@ export default function AddEmployee() {
                         </div>
                         <div className="px-2">
                             <div className="flex space-x-2 flex-wrap">
-                                <Input title="Nombre" type="text" value="" name="name" errMsj={actionData?.errors?.name}></Input>
-                                <Input title="Apelliddo" type="text" value="" name="lastname" errMsj={actionData?.errors?.lastname}></Input>
+                                <Input title="Nombre" type="text" value={data.name} name="name" errMsj={actionData?.errors?.name} onChange={handle((data,value)=>{data.name = value})}></Input>
+                                <Input title="Apelliddo" type="text" value={data.lastname} name="lastname" errMsj={actionData?.errors?.lastname} onChange={handle((data,value)=>{data.lastname = value})}></Input>
                             </div>
                             <div className="flex  space-x-2">
-                                <div className="w-28"><Input title="DNI" type="text" value="" name="dni" errMsj={actionData?.errors?.dni} ></Input></div>
-                                <div className="w-20"><Input title="Nro de legajo" type="text" value="" name="nrolegajo" errMsj={actionData?.errors?.nrolegajo}></Input></div>
+                                <div className="w-28"><Input title="DNI" type="text" value={data.dni} name="dni" errMsj={actionData?.errors?.dni} onChange={handle((data,value)=>{data.dni = value})}></Input></div>
+                                <div className="w-20"><Input title="Nro de legajo" type="text" value={data.employeeId} name="nrolegajo" errMsj={actionData?.errors?.nrolegajo}  onChange={handle((data,value)=>{data.employeeId = value})}></Input></div>
                             </div>
                             <div className="flex  space-x-2">
-                                <div className="w-28"><Input title="Celular" type="text" value="" name="celular" errMsj={actionData?.errors?.celular}></Input></div>
-                                <div className="flex-grow"><Input title="Dirección" type="text" value="" name="direccion" errMsj={actionData?.errors?.direccion}></Input></div>
+                                <div className="w-28"><Input title="Celular" type="text" value={data.phone} name="celular" errMsj={actionData?.errors?.celular} onChange={handle((data,value)=>{data.phone = value})}></Input></div>
+                                <div className="flex-grow"><Input title="Dirección" type="text" value={data.direccion} name="direccion" errMsj={actionData?.errors?.direccion} onChange={handle((data,value)=>{data.direccion = value})}></Input></div>
                             </div>
                         </div>
                         <div>
@@ -129,11 +167,11 @@ export default function AddEmployee() {
                         </div>
                         <div className="px-2">
                             <div className="flex  space-x-2">
-                                <Input title="Nombre de usuario" type="text" value="" name="user" errMsj={actionData?.errors?.user}></Input>
-                                <Input title="Correo electronico" type="text" value="" name="email" errMsj={actionData?.errors?.email}></Input>
+                                <Input title="Nombre de usuario" type="text" value={data.user.userName} name="user" errMsj={actionData?.errors?.user} onChange={handle((data,value)=>{data.user.userName = value})}></Input>
+                                <Input title="Correo electronico" type="text" value={data.user.email} name="email" errMsj={actionData?.errors?.email} onChange={handle((data,value)=>{data.user.email = value})}></Input>
                             </div>
-                            <div className="w-44"><Input title="Contraseña" type="text" value="" name="password" errMsj={actionData?.errors?.password}></Input></div>
-                            <Input title="Rol" type="text" value="" name="rol" errMsj={""}></Input>
+                            <div className="w-44"><Input title="Contraseña" type="text" value={""} name="password" errMsj={actionData?.errors?.password} onChange={handle((data,value)=>{data.user.password = value})}></Input></div>
+                            <Input title="Rol" type="text" value="" name="rol" errMsj={""} onChange={handle((data,value)=>{data.user.rol.id = value})}></Input>
                         </div>
                     </div>
                     <div className="float-right mt-2"><button>Confirm</button></div>
--- client/web/app/routes/manager/employee/$employeeId.tsx
+++ client/web/app/routes/manager/employee/$employeeId.tsx
@@ -1,13 +1,84 @@
-import { useOutletContext, useParams } from "@remix-run/react"
-import TitleDirectory from "~/components/TitleDirectory";
+import { ErrorBoundaryComponent, json, LoaderFunction, Response } from "@remix-run/node";
+import { useCatch, useLoaderData, useOutletContext, useParams } from "@remix-run/react"
+import { useEffect, useState } from "react";
+import DataLabel from "~/components/DataLabel";
+import ScreenCatch from "~/components/ScreenCatch";
+import { EmployeeModel, getEmployeeWhitId } from "~/server/employee.server";
+
+
+export const loader: LoaderFunction =async ({params,request}) => {
+    const url = new URL(request.url);
+    const created = url.searchParams.get("created");
+    const {employeeId} = params;
+    let employee: EmployeeModel;
+    if(employeeId === "-11"){
+        employee = {id: 0, dni:"123333", name:"Diego", lastname:"Noblega", phone:2333223, employeeId:2333, direccion:"", user:{email:"diego2212@gmail.com", userName:"diegoNoble", rol:{id:2}, password:"dwdwd"}}
+        return json({data:employee, created})
+    }
+    try {
+        let data:any = await getEmployeeWhitId(employeeId);
+        console.log(data)
+        console.log("hola")
+        if(data.error)
+            throw {error:data.error, status: data.status, message: data.message};
+        employee = data;
+        return json({data:employee, created})
+    } catch (error) {
+        console.log(error)
+        let e:any = error;
+        throw new Response(e.error, { status: e.status });
+    }
+    
+}
+
+export function CatchBoundary() {
+    const {data, status} = useCatch();
+    return (
+      <ScreenCatch data={data} status={status}></ScreenCatch>
+    );
+}
+
+export const ErrorBoundary: ErrorBoundaryComponent = ({error}) => {
+    return (<>
+    <div className="h-screen bg-app-msj-danger text-app-w-write flex flex-col justify-center items-center">
+        <span className="">{error.message}</span>
+    </div></>)
+ }
+
+
+
 
 export default function EmployeeId() {
-    const params = useParams();
-    const [title, setTitle] = useOutletContext<String>();
-
+    const {data:employee, created} = useLoaderData() as {data:EmployeeModel, created:string};
+    
     return (<>
-    <div>
-        <TitleDirectory directory={[title,"employee Id", params.employeeId!]}></TitleDirectory>
-    </div>
+        {(created)?<div className="p-2 bg-app-msj-accepted-pale flex">
+                <div className="w-1 rounded-sm bg-app-msj-accepted"></div>
+                <span className="ml-2">Empleado creado con exito</span>
+        </div>:<></>}
+        <div className="m-2">
+            <div className="text-2xl mb-2">
+                <h1>Panel de usuario - {employee.name} {employee.lastname}</h1>
+            </div>
+            <div className="bg-app-w-write border border-solid border-app-w-write-ash rounded-md p-4 flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 overflow-hidden">
+                <div className="flex flex-col space-y-4">
+                    <div className="flex sm:flex-col space-x-4 sm:space-x-0 sm:space-y-4">
+                        <DataLabel label={"Nombre"} text={employee.name}></DataLabel>
+                        <DataLabel label={"Apellido"} text={employee.lastname}></DataLabel>
+                        <DataLabel label={"Legajo"} text={employee.employeeId.toString()}></DataLabel>
+                    </div>
+                    <DataLabel label={"DNI"} text={employee.dni} className="w-full"></DataLabel>
+                </div>
+                <div className="bg-app-w-primary px-[1px] py-[1px] rounded-full"></div>
+                <div className="flex flex-col space-y-4">
+                    <DataLabel label={"Usuario"} text={employee.user.userName}></DataLabel>
+                    <DataLabel label={"Correo Electronico"} text={employee.user.email}></DataLabel>
+                    
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
     </>)
 }
