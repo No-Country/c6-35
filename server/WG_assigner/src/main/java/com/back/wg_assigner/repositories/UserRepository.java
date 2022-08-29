e88380ad9eece3c86bb4e978c07c17ac690900ed
Torales Andres <andres.toraless@gmail.com>
2022-08-28 22:33:50 -0300

    pegeable repositorio y servicio

--- server/WG_assigner/src/main/java/com/back/wg_assigner/repositories/UserRepository.java
+++ server/WG_assigner/src/main/java/com/back/wg_assigner/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.back.wg_assigner.repositories;
 
 import com.back.wg_assigner.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    public Page<User> findByDeleted(boolean deleted,Pageable pageable);
 }
+
+
