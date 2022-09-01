b4ce1252265ce771e6bc0b41b4b45d7ef527b06f
Noblega_Diego <diegomendoza20015@gmail.com>
2022-08-31 22:13:22 -0300

    add administración workTeam (#20)

--- client/web/app/components/Button.tsx
+++ client/web/app/components/Button.tsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom"
 
-export default function Button({text, type="default", onClick}:{text:String, type:string, onClick:Function|string}) {
+export enum TypeButton {
+    Confirm,
+    Cansel,
+    Default
+}
+
+export default function Button({text, type=TypeButton.Default, onClick}:{text:String, type:TypeButton, onClick:Function|string}) {
     let buttons:any = {
-        "confirm": `bg-[#FFC737] border-[#FCBA11] border-solid border-[1px] text-white rounded px-3 py-3 sm:py-0.5 text-[0.8rem] w-full block text-center`,
-        "cansel": `bg-[#FF5761] border-[#FF0000] border-solid border-[1px] text-white rounded px-3 py-3 sm:py-0.5 text-[0.8rem] w-full block text-center`,
-        "default": `bg-[#FBFBFB] border-[#F2F2F2] border-solid border-[1px] text-[#252B42] rounded px-3 py-3 sm:py-0.5  text-[0.8rem] w-full block text-center`}
+        0 : `bg-[#FFC737] border-[#FCBA11] border-solid border-[1px] text-white rounded px-3 py-3 sm:py-0.5 text-[0.8rem] w-full block text-center`,
+        1 : `bg-[#FF5761] border-[#FF0000] border-solid border-[1px] text-white rounded px-3 py-3 sm:py-0.5 text-[0.8rem] w-full block text-center`,
+        2 : `bg-[#FBFBFB] border-[#F2F2F2] border-solid border-[1px] text-[#252B42] rounded px-3 py-3 sm:py-0.5  text-[0.8rem] w-full block text-center`}
     
     
     return (<>
--- client/web/app/components/Table.tsx
+++ client/web/app/components/Table.tsx
@@ -1,6 +1,8 @@
 export interface column{
     name:String,
-    key:string
+    key:string,
+    callBack?:(obj:any)=>JSX.Element,
+    on:boolean
 }
 
 export default function Table({colums, data, row=10, bts}:{colums:Array<column>, data:Array<any>, row:number, bts:{btn:JSX.Element,action: Function, key:string}[]}) {
@@ -11,11 +13,11 @@ export default function Table({colums, data, row=10, bts}:{colums:Array<column>,
     }
     rows.push(...data,...vacio)
     return (<>
-    <div className="overflow-x-scroll w-full rounded-md  border-solid border-l-2 border-r-2 border-app-w-secodary" style={{scrollbarWidth: "none"}}>
+    <div className="overflow-x-scroll w-full rounded-md  border-solid border-2 border-app-w-secodary" style={{scrollbarWidth: "none"}}>
         <table className="w-full ">
             <thead>
                 <tr className="bg-app-w-secodary text-app-w-write-gris h-11 text-left" >
-                    {colums.map((col,i)=><th key={i} className="px-2 max-w-[300px]">{col.name}</th>)}
+                    {colums.map((col,i)=>(col.on)?<th key={i} className="px-2 max-w-[300px]">{col.name}</th>:<></>)}
                     
                     <th className="sticky right-0 bg-slate-400" style={{
                         background: "linear-gradient(90deg, rgba(2,0,36,0) 0%, #252B42 100%)"}}></th>
@@ -25,12 +27,18 @@ export default function Table({colums, data, row=10, bts}:{colums:Array<column>,
             <tbody>
                 {rows.map((obj, a)=>
                     <tr key={a} className="mr-20 text-app-w-write-ash border-solid border-[#EDF2F4] border-b-[1px] h-10 bg-app-w-write hover:bg-[#FAFCFD] hover:text-app-w-primary">
-                        {colums.map((col, i)=><td key={i} className="px-2 text-app-w-write-ash">{(obj !== null)? obj[col.key]: ""}</td>)}
+                        {colums.map((col, i)=>(col.on)?
+                            <td key={i} className="px-2 text-app-w-write-ash">{
+                                (col.callBack && obj !== null)? col.callBack(obj):
+                                ((obj !== null)? obj[col.key]: "")
+                                }
+                                </td>:
+                            <></>)}
                         
                         {(obj !== null)? <td className="sticky right-0 bg-app-w-write-gris border-solid border-l-[1px] border-app-w-write-back h-full min-w-[33px] w-10">
                             <div className="flex h-full px-2 space-x-4">
                                 {bts.map((btn, i)=>{
-                                    return <button key={i} onClick={()=>{btn.action(obj[btn.key])}}>{btn.btn}</button>
+                                    return <button key={i} onClick={(e)=>{e.preventDefault();btn.action(obj[btn.key])}}>{btn.btn}</button>
                                 })}
                             </div>
                         </td> : <td></td>}
